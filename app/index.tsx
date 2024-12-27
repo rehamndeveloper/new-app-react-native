750cff5353a5f08fd5b24657c0215682ef0ef8ea
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2024-12-27 10:20:22 +0500

    ..

--- app/index.tsx
+++ app/index.tsx
@@ -12,7 +12,7 @@ const Page = () => {
       <ImageBackground source={require('@/assets/images/getting-started.jpg')} style={{ flex: 1 }} resizeMode="cover" />
       <View style={styles.wrapper}>
         <Animated.Text style={styles.title} entering={FadeInRight.delay(300).duration(500)}>stay Updated!</Animated.Text>
-        <Animated.Text style={styles.description}>Get breaking news and presonalized updates drectly to your feed.</Animated.Text>
+        <Animated.Text style={styles.description} >Get breaking news and presonalized updates drectly to your feed.</Animated.Text>
         <Animated.View>
         <TouchableOpacity style={styles.btn} onPress={() => router.replace("/(tabs)")}>
           <Text style={styles.btnText}>Get Started</Text>
