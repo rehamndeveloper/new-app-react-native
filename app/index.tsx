386bda6fa65ac4eb4716d0cd8e0884ccb8b34098
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2024-12-27 09:55:28 +0500

    ..

--- app/index.tsx
+++ app/index.tsx
@@ -11,7 +11,7 @@ const Page = () => {
       <ImageBackground source={require('@/assets/images/getting-started.jpg')} style={{ flex: 1 }} resizeMode="cover" />
       <View style={styles.wrapper}>
         <Text style={styles.title}>stay Updated!</Text>
-        <Text style={styles.description}>stay Updated!</Text>
+        <Text style={styles.description}>Get breaking news and presonalized updates drectly to </Text>
         <TouchableOpacity onPress={() => router.replace("/(tabs)")}>
           <Text>Go to Home Screen</Text>
         </TouchableOpacity>
@@ -43,6 +43,6 @@ const styles = StyleSheet.create({
      textAlign: 'center',
   },
   description:{
-    
+
   }
 });
