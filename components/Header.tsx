8256aa58e2647476952a6d9c1ac6bcd4dcbea4e8
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2024-12-28 09:56:59 +0500

    Initial commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,37 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import { Ionicons } from '@expo/vector-icons'
+import { Colors } from '@/constants/Colors'
 
 type Props = {}
 
 const Header = (props: Props) => {
-  return (
-    <View style={styles.container} >
-     <Image source={{uri: 'https://xsgames.co/randomusers/avatar.php?g=male'}} style={styles.userImg}/>
-    </View>
-  )
+    return (
+        <View style={styles.container} >
+            <Image
+             source={{ uri: 'https://xsgames.co/randomusers/avatar.php?g=male' }} 
+             style={styles.userImg}
+            />
+            <TouchableOpacity onPress={() => { }}>
+                <Ionicons name='notifications-outline' size={24} color={Colors.black} />
+            </TouchableOpacity>
+        </View>
+    )
 }
 
 export default Header
 
 const styles = StyleSheet.create({
-    
+    container: {
+        paddingHorizontal: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    userImg: {
+        width: 50,
+        height: 50,
+        borderRadius: 30,
+    },
+
 })
