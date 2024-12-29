855a41aca5422ab708802e987727d4e93f1625bf
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2024-12-29 09:45:03 +0500

    ...

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -11,7 +11,7 @@ const Page = (props: Props) => {
 
   const getBreakingNews = async () => {
     try{
-      const URL = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY}&country=in&language=en&image=1`
+      const URL = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY}&country=in&language=en&image=1&removeduplicate=1&ize=5`
     }
   }
   return (
