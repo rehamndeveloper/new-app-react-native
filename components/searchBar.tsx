4d2ebc6eda42a202542b6264bac97976e71396c3
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2024-12-29 09:03:57 +0500

    ...

---
+++ components/searchBar.tsx
@@ -0,0 +1,32 @@
+import { StyleSheet, Text, TouchableOpacity, View, Image  } from 'react-native'
+import React from 'react'
+import { Ionicons } from '@expo/vector-icons'
+import { Colors } from '@/constants/Colors'
+
+type Props = {}
+
+const Header = (props: Props) => {
+    return (
+        <View style={styles.container} >
+            <View style={styles.userInfo}>
+                <Image
+                    source={{ uri: 'https://xsgames.co/randomusers/avatar.php?g=male' }}
+                    style={styles.userImg}
+                />
+                <View style={{gap: 3 }}>
+                    <Text style={styles.welcomeText}>Welcome</Text>
+                    <Text style={styles.userName}>Muhammad Sarfaraz Tahir</Text>
+                </View>
+            </View>
+            <TouchableOpacity onPress={() => { }}>
+                <Ionicons name='notifications-outline' size={24} color={Colors.black} />
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+export default Header;
+
+const styles = StyleSheet.create({
+
+})
