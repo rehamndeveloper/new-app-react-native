0915362f0a6c66758c1950852499324949d68e28
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2024-12-29 08:39:16 +0500

    ..

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View, Image  } from 'react-native'
 import React from 'react'
 import { Ionicons } from '@expo/vector-icons'
 import { Colors } from '@/constants/Colors'
