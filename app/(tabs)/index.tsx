420c8acdaa1baa76f1bf171bba0ecdcf77e7f2e9
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2024-12-29 09:47:40 +0500

    ..

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -14,6 +14,7 @@ const Page = (props: Props) => {
       const URL = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY}&country=in&language=en&image=1&removeduplicate=1&size=5`
     }
   }
+  
   return (
     <View style={[styles.container, {paddingTop: safeTop}]}>
      <Header />
