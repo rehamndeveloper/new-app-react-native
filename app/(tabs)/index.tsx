670698d25cbd02af9008f9614c51303a4865df21
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2024-12-29 09:03:20 +0500

    ...

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import Header from '@/components/Header'
+import { SearchBar } from 'react-native-screens'
 
 type Props = {}
 
@@ -10,6 +11,7 @@ const Page = (props: Props) => {
   return (
     <View style={[styles.container, {paddingTop: safeTop}]}>
      <Header />
+     <SearchBar />
     </View>
   )
 }
