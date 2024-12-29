cd35caae5933a3d24859718b5f5e9d16104aaa6b
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2024-12-29 09:35:05 +0500

    ...

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -10,7 +10,7 @@ const Page = (props: Props) => {
   const {top: safeTop } = useSafeAreaInsets();
 
   const getBreakingNews = async () => {
-    
+    try{}
   }
   return (
     <View style={[styles.container, {paddingTop: safeTop}]}>
