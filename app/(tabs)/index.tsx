c32479f5af6372234ca9fb000ea2b49371f779db
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2024-12-29 09:58:32 +0500

    .............

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -27,7 +27,9 @@ const Page = (props: Props) => {
     <View style={[styles.container, {paddingTop: safeTop}]}>
      <Header />
      <SearchBar />
-     {BreakingNews.map((item, inde ) => {})}
+     {BreakingNews.map((item, index ) => {
+      <Text>{item.title}</Text>
+     })}
     </View>
   )
 }
