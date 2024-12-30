9ee2f99c2e4850c86b72c5b34a0fe5660f80f289
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2024-12-30 09:40:47 +0500

    .........

--- components/SliderItem.tsx
+++ components/SliderItem.tsx
@@ -1,20 +1,21 @@
 import { NewsDataType } from '@/types'
 import React from 'react'
-import { StyleSheet, Text, TouchableOpacity, View, Image, Dimensions  } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View, Image, Dimensions } from 'react-native'
 
 type Props = {
     SlideItem: NewsDataType,
     index: number
 }
 
-const {width} = Dimensions.get('screen')
-const SliderItem = ({SlideItem , index}: Props) => {
+const { width } = Dimensions.get('screen')
+const SliderItem = ({ SlideItem, index }: Props) => {
     return (
         <View style={styles.itemWrapper} >
-           <Image 
-           source={{uri: SlideItem.image_url}}
-           style={styles.image}
-/>
+            <Image
+                source={{ uri: SlideItem.image_url }}
+                style={styles.image}
+            />
+            <Text></Text>
         </View>
     )
 }
@@ -23,14 +24,14 @@ export default SliderItem;
 
 const styles = StyleSheet.create({
     itemWrapper: {
-       position: 'relative',
-       width: width,
-       justifyContent: 'center',
-       alignContent: 'center',
+        position: 'relative',
+        width: width,
+        justifyContent: 'center',
+        alignContent: 'center',
     },
-    image:{
-       width: width,
-       height: 180,
-       borderRadius: 20,
+    image: {
+        width: width,
+        height: 180,
+        borderRadius: 20,
     },
 })
