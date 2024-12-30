3466fe2a58e8363e012cd29cebd18c5e574dd95e
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2024-12-30 10:24:44 +0500

    ....

--- components/BreakingNews.tsx
+++ components/BreakingNews.tsx
@@ -3,6 +3,7 @@ import { NewsDataType } from '@/types'
 import React, { useState } from 'react'
 import { FlatList, StyleSheet, Text, View } from 'react-native'
 import SliderItem from '@/components/SliderItem'
+import Animated, { useAnimatedRef, useSharedValue } from 'react-native-reanimated'
 
 type Props = {
     newsList: Array<NewsDataType>
@@ -12,6 +13,8 @@ type Props = {
 const BreakingNews = ({newsList}: Props) => {
     const [data, setData] = useState(newsList);
     const [paginationIndex, setPaginationIndex] = useState(0);
+    const scrollX = useSharedValue(0);
+    const ref = useAnimatedRef<Animated.FlatList<any>>();
     
     return (
         <View style={styles.container}>
