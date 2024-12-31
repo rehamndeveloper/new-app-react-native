b9dd47e2e46683a813eab01a786587ea5aaaf8f7
muhammadsarfaraztahir <sarfaraztahirarain2006@gmail.com>
2024-12-31 12:46:51 +0500

    ...

--- components/SliderItem.tsx
+++ components/SliderItem.tsx
@@ -3,7 +3,7 @@ import { NewsDataType } from '@/types'
 import { LinearGradient } from 'expo-linear-gradient'
 import React from 'react'
 import { StyleSheet, Text, TouchableOpacity, View, Image, Dimensions } from 'react-native'
-import { SharedValue } from 'react-native-reanimated'
+import { Extrapolation, interpolate, SharedValue, useAnimatedStyle } from 'react-native-reanimated'
 
 type Props = {
     SlideItem: NewsDataType,
@@ -13,6 +13,20 @@ type Props = {
 
 const { width } = Dimensions.get('screen')
 const SliderItem = ({ SlideItem, index, scrollX }: Props) => {
+    const rnStyle = useAnimatedStyle(()=>{
+        return{
+            transform: [
+                {
+                    translateX: interpolate(
+                        scrollX.value,
+                        [(index - 1) * width, index * width, (index + 1 ) * width],
+                        [-width * 0.15, 0, width * 0.15],
+                        Extrapolation.CLAMP
+                    )
+                }
+            ]
+        }
+    } )
     return (
         <View style={styles.itemWrapper} >
             <Image
