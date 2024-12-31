4e48031e61443c85f61b922ddeccd3928be9d13e
Muhammad Faizan <fiazansuri855@gmail.com>
2024-12-31 14:47:13 +0500

    ..

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,13 @@
-import axios from 'axios'
-import { NewsDataType } from '@/types'
-import Header from '@/components/Header'
-import SearchBar from '@/components/SearchBar'
-import React, { useEffect, useState } from 'react'
-import BreakingNews from '@/components/BreakingNews'
-import { ActivityIndicator, StyleSheet, Text, View } from 'react-native'
-import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import axios from "axios";
+import { NewsDataType } from "@/types";
+import Header from "@/components/Header";
+import SearchBar from "@/components/searchBar";
+import React, { useEffect, useState } from "react";
+import BreakingNews from "@/components/BreakingNews";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-type Props = {}
+type Props = {};
 
 const Page = (props: Props) => {
   const { top: safeTop } = useSafeAreaInsets();
@@ -15,12 +15,12 @@ const Page = (props: Props) => {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    getBreakingNews()
+    getBreakingNews();
   }, []);
 
   const getBreakingNews = async () => {
     try {
-      const URL = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY}&country=in&language=en&image=1&removeduplicate=1&size=5`
+      const URL = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY}&country=in&language=en&image=1&removeduplicate=1&size=5`;
       const response = await axios.get(URL);
 
       if (response && response.data) {
@@ -28,22 +28,22 @@ const Page = (props: Props) => {
         setIsLoading(false);
       }
     } catch (err: any) {
-      console.log('Error Message: ', err.message);
+      console.log("Error Message: ", err.message);
     }
-  }
+  };
 
   return (
     <View style={[styles.container, { paddingTop: safeTop }]}>
       <Header />
       <SearchBar />
       {isLoading ? (
-        <ActivityIndicator size={'large'} />
+        <ActivityIndicator size={"large"} />
       ) : (
         <BreakingNews newsList={breakingNews} />
       )}
     </View>
-  )
-}
+  );
+};
 
 export default Page;
 
@@ -51,4 +51,4 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-})
+});
--- components/BreakingNews.tsx
+++ components/BreakingNews.tsx
@@ -1,72 +1,99 @@
-import { Colors } from '@/constants/Colors'
-import { NewsDataType } from '@/types'
-import React, { useState } from 'react'
-import { FlatList, StyleSheet, Text, View } from 'react-native'
-import SliderItem from '@/components/SliderItem'
-import Animated, { useAnimatedRef, useAnimatedScrollHandler, useSharedValue } from 'react-native-reanimated'
-import Pagination from '@/components/pagination'
+import { Colors } from "@/constants/Colors";
+import { NewsDataType } from "@/types";
+import React, { useRef, useState } from "react";
+import { FlatList, StyleSheet, Text, View, ViewToken } from "react-native";
+import SliderItem from "@/components/SliderItem";
+import Animated, {
+  useAnimatedRef,
+  useAnimatedScrollHandler,
+  useSharedValue,
+} from "react-native-reanimated";
+import Pagination from "@/components/pagination";
 
 type Props = {
-    newsList: Array<NewsDataType>;
-}
+  newsList: Array<NewsDataType>;
+};
 
+const BreakingNews = ({ newsList }: Props) => {
+  const [data, setData] = useState(newsList);
+  const [paginationIndex, setPaginationIndex] = useState(0);
+  const scrollX = useSharedValue(0);
+  const ref = useAnimatedRef<Animated.FlatList<any>>();
 
-const BreakingNews = ({newsList}: Props) => {
-    const [data, setData] = useState(newsList);
-    const [paginationIndex, setPaginationIndex] = useState(0);
-    const scrollX = useSharedValue(0);
-    const ref = useAnimatedRef<Animated.FlatList<any>>();
-    
-    const onScrollHandler = useAnimatedScrollHandler({
-        onScroll: (e) => {
-            scrollX.value = e.contentOffset.x;
-        },
-    });
+  const onScrollHandler = useAnimatedScrollHandler({
+    onScroll: (e) => {
+      scrollX.value = e.contentOffset.x;
+    },
+  });
 
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Breaking New</Text>
-            <View style={styles.slideWrapper}>
-                <Animated.FlatList 
-                ref={ref}
-                 data={data} 
-                 keyExtractor={(_, index) => `list_item${index}`} 
-                 renderItem={({ item , index })=>{
-                    <SliderItem slideItem={item} index={index}  scrollX={scrollX}/>
-                }}
-                horizontal
-                showsHorizontalScrollIndicator={false}
-                pagingEnabled
-                onScroll={onScrollHandler}
-                scrollEventThrottle={16}
-                onEndReachedThreshold={0.5}
-                onEndReached={()=> setData([...data, ...newsList])}
-                viewabilityConfigCallbackPairs={
-                    viewabilityConfigCallbackPairs.current
-                }
-                />
-                <Pagination  items={newsList} scrollX={scrollX} paginationIndex={paginationIndex}/>
-            </View>
-        </View>
-    )
-}
+  const onviewableItemsChanged = ({
+    viewableItems,
+  }: {
+    viewableItems: ViewToken[];
+  }) => {
+    if (
+      viewableItems[0].index !== undefined &&
+      viewableItems[0].index !== null
+    ) {
+      setPaginationIndex(viewableItems[0].index % newsList.length);
+    }
+  };
+
+  const viewabilityConfig = {
+    itemVisiblePercentThreshold: 50,
+  };
+  const viewabilityConfigCallbackPairs = useRef([
+    { viewabilityConfig, onviewableItemsChanged },
+  ]);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Breaking New</Text>
+      <View style={styles.slideWrapper}>
+        <Animated.FlatList
+          ref={ref}
+          data={data}
+          keyExtractor={(_, index) => `list_item${index}`}
+          renderItem={({ item, index }) => {
+            <SliderItem slideItem={item} index={index} scrollX={scrollX} />;
+          }}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          pagingEnabled
+          onScroll={onScrollHandler}
+          scrollEventThrottle={16}
+          onEndReachedThreshold={0.5}
+          onEndReached={() => setData([...data, ...newsList])}
+          viewabilityConfigCallbackPairs={
+            viewabilityConfigCallbackPairs.current
+          }
+        />
+        <Pagination
+          items={newsList}
+          scrollX={scrollX}
+          paginationIndex={paginationIndex}
+        />
+      </View>
+    </View>
+  );
+};
 
 export default BreakingNews;
 
 const styles = StyleSheet.create({
-    container:{
-        marginBottom: 10,
-    },
-    title:{
-        fontSize: 10,
-        fontWeight: "600",
-        color: Colors.black,
-        marginBottom: 10,
-        marginLeft: 20,
-    },
-    slideWrapper:{
-        // width: '100%',
-        // flex: 1,
-        justifyContent: 'center',
-    },
+  container: {
+    marginBottom: 10,
+  },
+  title: {
+    fontSize: 10,
+    fontWeight: "600",
+    color: Colors.black,
+    marginBottom: 10,
+    marginLeft: 20,
+  },
+  slideWrapper: {
+    // width: '100%',
+    // flex: 1,
+    justifyContent: "center",
+  },
 });
